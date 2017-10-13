445aa339d4a297c19f6072389dcf3a452a9e01e5
Firefly4266 <gaines.johnp@gmail.com>
2017-10-12 22:32:22 -0700

    Problem 36 completed.

--- Problem36/Problem36.cs
+++ Problem36/Problem36.cs
@@ -9,21 +9,38 @@ namespace Problem36
     class A
     {
         public int num = 1;
-        public void ADisplay()
+        public virtual void Display()
         {
-            Console.WriteLine($"     Class A's variable is {num}\n");
+            Console.WriteLine(num);
         }
     }
     class B : A
     {
-        private int numB = 5;
+        private int num = 5;
 
-        public void BDisplay()
+        public override void Display()
         {
-            Console.WriteLine($"     Class A's variable is {num}");
-            Console.WriteLine($"     Class B's variable is {numB}\n");
+            Console.WriteLine(num);
         }
     }
+
+    class C : B
+    {
+        private int num = 2;
+        public void Display()
+        {
+            Console.WriteLine(num);
+        }
+    }
+
+    class D : C
+    {
+        public void Display()
+        {
+            Console.WriteLine(num);
+        }
+    }
+     
 
     public class Class36
     {
@@ -31,11 +48,17 @@ namespace Problem36
         {
             A MyA = new A();
             B MyB = new B();
+            C MyC = new C();
+            D MyD = new D();
 
-            Console.WriteLine($"Class A contains:");
-            MyA.ADisplay();
-            Console.WriteLine($"Class B contains:");
-            MyB.BDisplay();
+            Console.Write($"\n Class A contains:");
+            MyA.Display();
+            Console.Write($"\n Class A's variable in class B is ");
+            MyB.Display();
+            Console.Write($"\n Class A's variable in class C is ");
+            MyC.Display();
+            Console.Write($"\n Class A's variable in class D is ");
+            MyD.Display();
         }
     }
 
